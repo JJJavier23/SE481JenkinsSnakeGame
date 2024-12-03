231b86caae91bdbb5719ce1c9a05ab9a181ad109
dheerajsingavarapu <dsingavarapu@gmail.com>
2024-12-02 20:08:41 -0800

    Intentionally failing test to demonstrate test detection

--- src/test/java/org/psnbtech/BoardPanelTest.java
+++ src/test/java/org/psnbtech/BoardPanelTest.java
@@ -7,7 +7,7 @@ public class BoardPanelTest {
 
     @Test
     public void testTileSize() {
-        assertEquals(20, BoardPanel.TILE_SIZE, "Tile size should be 20 pixels");
+        assertEquals(30, BoardPanel.TILE_SIZE, "Tile size should be 30 pixels");
     }
 
     @Test
@@ -18,8 +18,8 @@ public class BoardPanelTest {
 
     @Test
     public void testBoardSize() {
-        int expectedWidth = BoardPanel.TILE_SIZE * BoardPanel.COL_COUNT;  // 20 * 25 = 500
-        int expectedHeight = BoardPanel.TILE_SIZE * BoardPanel.ROW_COUNT; // 20 * 25 = 500
+        int expectedWidth = BoardPanel.TILE_SIZE * BoardPanel.COL_COUNT;  
+        int expectedHeight = BoardPanel.TILE_SIZE * BoardPanel.ROW_COUNT; 
         
         assertEquals(500, expectedWidth, "Board width should be 500 pixels");
         assertEquals(500, expectedHeight, "Board height should be 500 pixels");
